5986f7fb8683b2253b2a1b7cdec31c5c29dac7b8
s016893 <s016893@lm-23s016893.lmsd.org>
2021-03-11 09:41:27 -0500

    trying to get mode to recognize multiple numbers. revert if this doesn't work

--- Eclipse 2/Strings and Wrapper Classes/src/StringsAndWrapperClassesMain.java
+++ Eclipse 2/Strings and Wrapper Classes/src/StringsAndWrapperClassesMain.java
@@ -3,6 +3,8 @@
 //Date: Mar 2, 2021
 //Written By: Ari Steinfeld
 import java.util.Arrays;
+import java.util.ArrayList;
+
 import java.util.Scanner;
 public class StringsAndWrapperClassesMain {
 
@@ -12,13 +14,13 @@ public class StringsAndWrapperClassesMain {
 		//project one. read in a simple phrase asking what the user's phrase is, and turn it into a modified version of it.
 		System.out.println("Please enter in a string: ");
 		String input = s.nextLine();
-		
+
 		//turns the string into an array of chars to make it easier to replace individual letters
 		char[] inputChar = input.toCharArray();
 		for (int i = 0; i < input.length(); i++) {
 			if ( inputChar[i] < 90 && inputChar[i] > 64) {
 				inputChar[i] = (char) (inputChar[i] + 32);
-				
+
 			}
 			else if ( inputChar[i] < 123 && inputChar[i] > 96) {
 				inputChar[i] = (char) (inputChar[i] - 32);
@@ -26,7 +28,7 @@ public class StringsAndWrapperClassesMain {
 		}
 		//turns the array of chars back into a string
 		input = String.valueOf(inputChar);
-		
+
 		//replaces all spaces with a zero width joiner character
 		input = input.replace(' ', '‍');
 		//replaces all es with is
@@ -51,46 +53,79 @@ public class StringsAndWrapperClassesMain {
 		double newPrice =1.10 * Double.parseDouble(price);
 		String firstPart = noahRubin.substring(0, noahRubin.indexOf('$')+1);
 		System.out.println(firstPart + newPrice + secondPart);
-		
+
 		//PROJECT 3: get passed an unkown ammount of int in a string and get the mean, median, and mode
 		System.out.println("Enter the numbers: ");
 		String number = s.nextLine();
 		String[] numberArr = number.split(" ");
 		double[] numbers = new double[numberArr.length];
-		for (int i = 0; i < numberArr.length; i++) {
-			numbers[i] = (Double.parseDouble(numberArr[i]));
-		}
+
 		int counter = 0;
 		double mean = 0, median = 0;
-
+		ArrayList <Integer> numsArrList = new ArrayList<>();
+		ArrayList <Integer> counterList = new ArrayList<>();
+		ArrayList <Integer> modeList = new ArrayList<>();
+		boolean runRemove = false;
 		Arrays.sort(numbers);
+		for (int i = 0; i < numberArr.length; i++) {
+			numbers[i] = (Double.parseDouble(numberArr[i]));
+			numsArrList.add(Integer.parseInt(numberArr[i]));
+		}
 		for(int i = 0; i < numbers.length; i++) {
 			counter++;
 			mean+= numbers[i];
-			
+
 		}
 		int mode = 0, counter3 = 0, firstFor, secondFor;
-		for (firstFor = 0; firstFor < numbers.length; ++firstFor) {
-	         int count = 0;
-	         for (secondFor = 0; secondFor < numbers.length; ++secondFor) {
-	            if (numbers[secondFor] == numbers[firstFor])
-	            ++count;
-	         }
+		int count = 0;
+		for (int i = 0; i < numsArrList.size(); i++) {
+			runRemove = false;
+			mode = 0; counter3 = 0;
+			for (firstFor = 0; firstFor < numsArrList.size(); ++firstFor) {
+				count = 0;
+				for (secondFor = 0; secondFor < numsArrList.size(); ++secondFor) {
+					if (numsArrList.get(secondFor) == numsArrList.get(firstFor))
+						++count;
+				}
 
-	         if (count > counter3) {
-	            counter3 = count;
-	            mode = (int) numbers[firstFor];
-	         }
-	      }
+				if (count > counter3) {
+					counter3 = count;
+					mode = (int) numsArrList.get(firstFor);
+				}
+			}
+			if (modeList.size() == 0) {
+				modeList.add(mode);
+				runRemove = true;
+				counterList.add(count);
+			} else if (count == counterList.get(0)) {
+				modeList.add(mode);
+				runRemove = true;
+				counterList.add(count);
+			}
+			if (runRemove){
+				for (int j = 0; j < numsArrList.size(); j++){
+					if (numsArrList.get(j) == mode) {
+						numsArrList.remove(j);
+					}
+				}
+			}
+
+
+
+
+		}
 		median = numbers[(int)counter/2];
 		mean = mean/counter;
 		System.out.println("The mean is:  " + mean);
 		System.out.println("The median is:  " + median);
-		System.out.println("The mode is:  " + mode );
-				
-		
-		
-		
+		System.out.print("The mode is:  ");
+		for (int i = 0; i < modeList.size(); i++) {
+			System.out.print(modeList.get(i) + " ");
+		}
+
+
+
+
 
 	}
 

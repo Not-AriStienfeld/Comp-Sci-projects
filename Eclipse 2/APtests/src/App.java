53c39e283554d8de2c6521d845ec23e7c169723f
s016893 <s016893@192.168.4.33>
2021-03-18 00:10:22 -0400

    its fucking midnight I should be asleep

--- Eclipse 2/APtests/src/App.java
+++ Eclipse 2/APtests/src/App.java
@@ -2,6 +2,31 @@
 import java.util.ArrayList;
 public class App {
     public static void main(String[] args) throws Exception {
-        
+    	int numbers[] = {0, 1, 2};
+    	double sum = 0;
+    	int min = numbers[0];
+    	int max = numbers[0];
+    	for (int i = 0; i < numbers.length; i++) {
+    		if (numbers[i] > max) {
+    			max =numbers[i];
+    		}
+    		if (numbers[i] < min) {
+    			min = numbers[i];
+    		}
+    	}
+    	System.out.println("max: " + max);
+    	System.out.println("min: " + min);
+    	
+    	String str = "hello";
+    	int shift = 5;
+    	char[] strArr = str.toCharArray(); 
+    	for (int i = 0; i < strArr.length; i++) {
+    		if (((int) strArr[i] +shift < 91 && (int) strArr[i] > 64) || ((int) strArr[i]+shift < 123 && (int) strArr[i] > 96)) {
+    			strArr[i] = (char) (strArr[i] +shift);
+    		}else {
+    			strArr[i] = (char) (strArr[i] +shift -32);
+    		}
+    	}
+    	
     }
 }

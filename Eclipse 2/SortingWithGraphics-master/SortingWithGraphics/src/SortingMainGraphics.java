9c18c38c6784350156c11b8aa70bd91a79d7f068
Not-AriStienfeld <crazybonesx@gmail.com>
2021-04-26 21:13:35 -0400

    made stuff work with VScode

--- Eclipse 2/SortingWithGraphics-master/SortingWithGraphics/src/SortingMainGraphics.java
+++ Eclipse 2/SortingWithGraphics-master/SortingWithGraphics/src/SortingMainGraphics.java
@@ -9,7 +9,7 @@ public class SortingMainGraphics extends JFrame{
 		SortingMainGraphics window = new SortingMainGraphics();
 	    JPanel p = new JPanel();
 	    p.add(new SortingPanel());  //  add a class that extends JPanel
-	    window.setTitle("Rename this After Your Sorting Algorithm");
+	    window.setTitle("Bubble Sort");
 	    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    window.setContentPane(p);
 	    window.pack();

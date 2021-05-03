82b14aa9dcfe58766ffd04591583c4799c9cdc41
s016893 <s016893@lm-23s016893.lmsd.org>
2021-05-03 10:53:21 -0400

    housekeeping

--- Eclipse 2/SortingWithGraphics-master/SortingWithGraphics/src/SortingMainGraphics.java
+++ Eclipse 2/SortingWithGraphics-master/SortingWithGraphics/src/SortingMainGraphics.java
@@ -9,7 +9,7 @@ public class SortingMainGraphics extends JFrame{
 		SortingMainGraphics window = new SortingMainGraphics();
 	    JPanel p = new JPanel();
 	    p.add(new SortingPanel());  //  add a class that extends JPanel
-	    window.setTitle("Bubble Sort");
+	    window.setTitle("I am cooler than you");
 	    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    window.setContentPane(p);
 	    window.pack();
